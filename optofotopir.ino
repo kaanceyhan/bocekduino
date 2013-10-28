96290866354c610b1b6aa780d851ab86944e19bf
Kaan CEYHAN <kaanceyhan@hotmail.com>
2013-10-28 22:54:48 +0200

    Create optofotopir.ino

---
+++ optofotopir.ino
@@ -0,0 +1,33 @@
+// Canon trigger
+// ePIR + 4N25 
+
+int ledPin = 13;
+int pirPin=7;
+int shutterPin = 9;
+int focusPin=11;
+int pirStatus=0;
+
+void setup(){
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(shutterPin, OUTPUT);
+  pinMode(focusPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  pirStatus=digitalRead(pirPin);
+  if (pirStatus == LOW){
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(focusPin, HIGH); 
+    delay(300);
+    digitalWrite(focusPin, LOW); 
+    delay(100);
+    digitalWrite(shutterPin, HIGH); 
+    delay(100);
+    digitalWrite(shutterPin, LOW); 
+    delay(300);
+    digitalWrite(ledPin, LOW);    
+    delay(900);    
+  }
+}
